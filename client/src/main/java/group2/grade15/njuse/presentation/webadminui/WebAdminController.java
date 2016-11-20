28a19c2455a0cccc2dc553a5718a98e60c10c576
FrodoLuo <359303713@qq.com>
2016-11-20 12:09:36 +0800

    Add files via upload

--- client/src/main/java/group2/grade15/njuse/presentation/webadminui/WebAdminController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webadminui/WebAdminController.java
@@ -1,4 +1,4 @@
-package main.java.group2.grade15.njuse.presentation.webadminui;
+package group2.grade15.njuse.presentation.webadminui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -27,19 +27,19 @@ public class WebAdminController {
     //messageButton的按钮实现
     @FXML
     public void message_movein(MouseEvent event){
-        messageImage.setImage(new Image("file:src/main/res/webadmin/message_movein.png"));
+        messageImage.setImage(new Image("file:client/src/main/res/webadmin/message_movein.png"));
     }
     @FXML
     public void message_moveout(MouseEvent event){
-        messageImage.setImage(new Image("file:src/main/res/webadmin/message.png"));
+        messageImage.setImage(new Image("file:client/src/main/res/webadmin/message.png"));
     }
     @FXML
     public void message_press(MouseEvent event){
-        messageImage.setImage(new Image("file:src/main/res/webadmin/message_clicked.png"));
+        messageImage.setImage(new Image("file:client/src/main/res/webadmin/message_clicked.png"));
     }
     @FXML
     public void message_release(MouseEvent event){
-        messageImage.setImage(new Image("file:src/main/res/webadmin/message_movein.png"));
+        messageImage.setImage(new Image("file:client/src/main/res/webadmin/message_movein.png"));
     }
 
 
@@ -47,58 +47,58 @@ public class WebAdminController {
     //settingButton的按钮实现
     @FXML
     public void setting_movein(MouseEvent event){
-        settingImage.setImage(new Image("file:src/main/res/webadmin/setting_movein.png"));
+        settingImage.setImage(new Image("file:client/src/main/res/webadmin/setting_movein.png"));
     }
 
     @FXML
     public void setting_moveout(MouseEvent event){
-        settingImage.setImage(new Image("file:src/main/res/webadmin/setting.png"));
+        settingImage.setImage(new Image("file:client/src/main/res/webadmin/setting.png"));
     }
     @FXML
     public void setting_press(MouseEvent event){
-        settingImage.setImage(new Image("file:src/main/res/webadmin/setting_press.png"));
+        settingImage.setImage(new Image("file:client/src/main/res/webadmin/setting_press.png"));
     }
     @FXML
     public void setting_release(MouseEvent event){
-        settingImage.setImage(new Image("file:src/main/res/webadmin/setting_movein.png"));
+        settingImage.setImage(new Image("file:client/src/main/res/webadmin/setting_movein.png"));
     }
 
     //AccountButton的按钮实现
     @FXML
     public void Account_movein(MouseEvent event){
-        accountImage.setImage(new Image("file:src/main/res/webadmin/Account_movein.png"));
+        accountImage.setImage(new Image("file:client/src/main/res/webadmin/Account_movein.png"));
     }
 
     @FXML
     public void Account_moveout(MouseEvent event){
-        accountImage.setImage(new Image("file:src/main/res/webadmin/Account.png"));
+        accountImage.setImage(new Image("file:client/src/main/res/webadmin/Account.png"));
     }
     @FXML
     public void Account_press(MouseEvent event){
-        accountImage.setImage(new Image("file:src/main/res/webadmin/Account_press.png"));
+        accountImage.setImage(new Image("file:client/src/main/res/webadmin/Account_press.png"));
     }
     @FXML
     public void Account_release(MouseEvent event){
-        accountImage.setImage(new Image("file:src/main/res/webadmin/Account_movein.png"));
+        accountImage.setImage(new Image("file:client/src/main/res/webadmin/Account_movein.png"));
     }
 
     //HotelButton的按钮实现
     @FXML
     public void Hotel_movein(MouseEvent event){
-        hotelImage.setImage(new Image("file:src/main/res/webadmin/Hotel_movein.png"));
+        hotelImage.setImage(new Image("file:client/src/main/res/webadmin/Hotel_movein.png"));
     }
 
     @FXML
     public void Hotel_moveout(MouseEvent event){
-        hotelImage.setImage(new Image("file:src/main/res/webadmin/Hotel.png"));
+        hotelImage.setImage(new Image("file:client/src/main/res/webadmin/Hotel.png"));
     }
     @FXML
     public void Hotel_press(MouseEvent event){
-        hotelImage.setImage(new Image("file:src/main/res/webadmin/Hotel_press.png"));
+        hotelImage.setImage(new Image("file:client/src/main/res/webadmin/Hotel_press.png"));
     }
     @FXML
     public void Hotel_release(MouseEvent event){
-        hotelImage.setImage(new Image("file:src/main/res/webadmin/Hotel_movein.png"));
+        hotelImage.setImage(new Image("file:client/src/main/res/webadmin/Hotel_movein.png"));
     }
 
 }
