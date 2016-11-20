3a4aa3a38034b502c84525fc63bf40601006409f
iriswuyuhan <1426546185@qq.com>
2016-11-20 17:14:39 +0800

    change customerdata; add factory and database

--- server/src/main/java/group2/grade15/njuse/data/customerdata/CustomerDataBaseImpl.java
+++ server/src/main/java/group2/grade15/njuse/data/customerdata/CustomerDataBaseImpl.java
@@ -1,18 +1,28 @@
 package group2.grade15.njuse.data.customerdata;
 
 
+import group2.grade15.njuse.data.databaseimpl.DatabaseInfo;
+import group2.grade15.njuse.data.databaseimpl.DatabaseMySql;
 import group2.grade15.njuse.dataservice.CustomerDataService;
 import group2.grade15.njuse.po.CustomerPO;
 import group2.grade15.njuse.utility.ResultMessage;
 
 import java.rmi.RemoteException;
+import java.sql.Connection;
 
 /**
  * Created by George on 2016/11/13.
  */
-public class CustomerData implements CustomerDataService {
+public class CustomerDataBaseImpl implements CustomerDataService {
+    private DatabaseMySql mySql=null;
+    private Connection customerConnection=null;
+
+    public CustomerDataBaseImpl(DatabaseInfo info) throws RemoteException{
+        mySql=new DatabaseMySql(info);
+    }
 
     public CustomerPO get(int customerId) throws RemoteException {
+
         return null;
     }
 
