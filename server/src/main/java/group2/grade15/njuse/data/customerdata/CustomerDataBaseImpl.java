8a2f5e67aac91570859d42b479f8661d3fe77a6b
iriswuyuhan <1426546185@qq.com>
2016-11-22 11:46:39 +0800

    Merge branches 'Wu' and 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Wu
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPromotionListPO.java
    #	common/src/main/java/group2/grade15/njuse/po/WebPromotionListPO.java

--- server/src/main/java/group2/grade15/njuse/data/customerdata/CustomerDataBaseImpl.java
+++ server/src/main/java/group2/grade15/njuse/data/customerdata/CustomerDataBaseImpl.java
@@ -9,10 +9,7 @@ import group2.grade15.njuse.utility.MemberType;
 import group2.grade15.njuse.utility.ResultMessage;
 
 import java.rmi.RemoteException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by George on 2016/11/13.
@@ -26,9 +23,15 @@ public class CustomerDataBaseImpl implements CustomerDataService {
         customerConnection=mySql.init();
     }
 
+    /**
+     * @param customerId
+     * @return CustomerPO
+     * @throws RemoteException
+     * 获取customer信息
+     */
     public CustomerPO get(int customerId) throws RemoteException {
         if(customerConnection==null){
-            return null;
+            customerConnection=mySql.init();
         }
 
         int id=0;
@@ -37,30 +40,73 @@ public class CustomerDataBaseImpl implements CustomerDataService {
         MemberType type=null;
         try{
             Statement getInfo=customerConnection.createStatement();
-            if(getInfo==null){
+            /*if(getInfo==null){
                 System.out.print("wrong");
-            }
+            }*/
             ResultSet r=getInfo.executeQuery(
                     "select * from Customer where customerID = "+customerId);
-            r.next();
-            id=customerId;
-            password=r.getString(2);
-            name=r.getString(3);
-            identity=r.getString(4);
-            contact=r.getString(5);
-            credit=r.getFloat(6);
-            type=MemberType.values()[r.getString(7).charAt(0)-'0'];
+            if(r.next()) {
+                id = customerId;
+                password = r.getString(2);
+                name = r.getString(3);
+                identity = r.getString(4);
+                contact = r.getString(5);
+                credit = r.getFloat(6);
+                type = MemberType.values()[r.getInt(7)];
+            }
             getInfo.close();
             customerConnection.close();
+            customerConnection=null;
         }catch(SQLException e){
             e.printStackTrace();
             return null;
         }
+
         CustomerPO customerInfo=new CustomerPO(id,name,password,contact,identity,credit,type);
         return customerInfo;
     }
 
-    public ResultMessage add(CustomerPO po) throws RemoteException {
+    /**
+     * @param po
+     * @return ResultMessage
+     * @throws RemoteException
+     * 仅name，password，contact，identity，type可进行添加，identity和type一经添加不可修改
+     */
+    public CustomerPO add(CustomerPO po) throws RemoteException {
+        if(customerConnection==null){
+            customerConnection=mySql.init();
+        }
+
+        int id=0;
+        String name=po.getName(),password=po.getPassword(),contact=po.getContact(),identity=po.getIdentityNum();
+        MemberType type=po.getType();
+        float credit=0;
+
+        try{
+            Statement makeID=customerConnection.createStatement();
+            ResultSet newID=makeID.executeQuery("select max(customerid) from customer");
+            if(newID.next()){
+                id=newID.getInt(1);
+            }
+            else{
+                throw new SQLException();
+            }
+            makeID.close();
+
+            PreparedStatement addOne=customerConnection.prepareStatement("insert into customer values(?,?,?,?,?,?,?)");
+            addOne.setInt(1,id);
+            addOne.setString(2,password);
+            addOne.setString(3,name);
+            addOne.setString(4,identity);
+            addOne.setString(5,contact);
+            addOne.setInt(7,type.ordinal());
+            addOne.executeUpdate();//尚未get新的po
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        CustomerPO newCustomer=new CustomerPO(id,name,password,contact,identity,credit,type);
         return null;
     }
 
