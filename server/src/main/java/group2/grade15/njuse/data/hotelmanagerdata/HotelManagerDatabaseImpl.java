686143ae620e8ce0e37deb48e6dbb6392c67f71a
iriswuyuhan <1426546185@qq.com>
2016-11-22 21:24:02 +0800

    Merge branches 'Wu' and 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Wu
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPromotionListPO.java
    #	common/src/main/java/group2/grade15/njuse/po/WebPromotionListPO.java

--- server/src/main/java/group2/grade15/njuse/data/hotelmanagerdata/HotelManagerDatabaseImpl.java
+++ server/src/main/java/group2/grade15/njuse/data/hotelmanagerdata/HotelManagerDatabaseImpl.java
@@ -15,58 +15,49 @@ import java.sql.Statement;
 /**
  * Created by ALIENWARE-PC on 2016/11/13.
  */
-public class HotelManagerDatabaseImpl implements HotelManagerDataService{
-    private DatabaseMySql mySql=null;
-    private Connection hotelManagerConnection=null;
+public class HotelManagerDatabaseImpl implements HotelManagerDataService {
+    private DatabaseMySql mySql = null;
+    private Connection hotelManagerConnection = null;
 
-    public HotelManagerDatabaseImpl(DatabaseInfo info) throws RemoteException{
-        mySql=new DatabaseMySql(info);
-        hotelManagerConnection=mySql.init();
+    public HotelManagerDatabaseImpl(DatabaseInfo info) throws RemoteException {
+        mySql = new DatabaseMySql(info);
+        hotelManagerConnection = mySql.init();
     }
 
     public HotelManagerPO get(int hotelManagerId) throws RemoteException {
-        if(hotelManagerConnection==null){
-            hotelManagerConnection=mySql.init();
+        if (hotelManagerConnection == null) {
+            hotelManagerConnection = mySql.init();
         }
 
-        int id=0;
-        String password=null,name=null,contact=null;
-        int hotelID=0;
-        try{
-            Statement getInfo=hotelManagerConnection.createStatement();
-            ResultSet r=getInfo.executeQuery("select * from hotelmanager where hotelmanagerid = "+hotelManagerId);
-            if(r.next()){
-                id=hotelManagerId;
-                password=r.getString(2);
-                name=r.getString(3);
-                contact=r.getString(4);
-                hotelID=r.getInt(5);
-            }
-            else{
+        int id = 0;
+        String password = null, name = null, contact = null;
+        int hotelID = 0;
+        try {
+            Statement getInfo = hotelManagerConnection.createStatement();
+            ResultSet r = getInfo.executeQuery("select * from hotelmanager where hotelmanagerid = " + hotelManagerId);
+            if (r.next()) {
+                id = hotelManagerId;
+                password = r.getString(2);
+                name = r.getString(3);
+                contact = r.getString(4);
+                hotelID = r.getInt(5);
+            } else {
                 throw new SQLException();
             }
 
             getInfo.close();
             hotelManagerConnection.close();
-            hotelManagerConnection=null;
-        }catch (SQLException e){
+            hotelManagerConnection = null;
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
 
-        HotelManagerPO hotelManagerPO=new HotelManagerPO(id,password,name,contact,hotelID);
+        HotelManagerPO hotelManagerPO = new HotelManagerPO(id, password, name, contact, hotelID);
         return hotelManagerPO;
     }
 
-    public ResultMessage add(HotelManagerPO po) throws RemoteException {
-        return null;
-    }
-
     public ResultMessage modify(HotelManagerPO po) throws RemoteException {
-        return null;
-    }
-
-    public ResultMessage remove(int hotelManagerId) throws RemoteException {
         return null;
     }
 }
