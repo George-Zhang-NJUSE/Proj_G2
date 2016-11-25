37fed2360e4283e8b73a7fa7a4a904ecde59b765
Luo <359303713@qq.com>
2016-11-25 23:49:48 +0800

    update of the webadmin Main gui

--- client/src/main/java/group2/grade15/njuse/presentation/webadminui/WebAdminController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webadminui/WebAdminController.java
@@ -1,10 +1,12 @@
 package group2.grade15.njuse.presentation.webadminui;
 
+import group2.grade15.njuse.presentation.myanimation.Fade;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 /**
  * Created by ALIENWARE-PC on 2016/11/19.
@@ -22,6 +24,22 @@ public class WebAdminController {
     private ImageView accountImage;
     @FXML
     private ImageView hotelImage;
+    @FXML
+    private ImageView HMBack;
+    @FXML
+    private ImageView AMBack;
+    @FXML
+    private ImageView addAccount;
+    @FXML
+    private ImageView addHotel;
+    @FXML
+    private Label addAccountLabel;
+    @FXML
+    private GridPane accountManage;
+    @FXML
+    private GridPane hotelManage;
+    @FXML
+    private GridPane welcome;
 
 
     //messageButton的按钮实现
@@ -87,6 +105,14 @@ public class WebAdminController {
     public void Account_release(MouseEvent event) {
         accountImage.setImage(new Image("file:client/src/main/res/webadmin/Account_movein.png"));
     }
+    @FXML
+    public void Account_action(MouseEvent event){
+        Fade amFade=new Fade(accountManage,400,true);
+        amFade.play();
+        Fade welFadeIn=new Fade(welcome,400,false);
+        welFadeIn.play();
+    }
+
 
     //HotelButton的按钮实现
     @FXML
@@ -108,5 +134,105 @@ public class WebAdminController {
     public void Hotel_release(MouseEvent event) {
         hotelImage.setImage(new Image("file:client/src/main/res/webadmin/Hotel_movein.png"));
     }
+    @FXML
+    public void Hotel_action(MouseEvent event){
 
+        Fade hmFade=new Fade(hotelManage,400,true);
+        hmFade.play();
+        Fade welFadeIn=new Fade(welcome,400,false);
+        welFadeIn.play();
+    }
+
+    //AMBack的按钮实现
+    @FXML
+    public void AMBack_movein(MouseEvent event){
+        AMBack.setImage(new Image("file:client/src/main/res/webadmin/back_movein.png"));
+    }
+    @FXML
+    public void AMBack_moveout(MouseEvent event){
+        AMBack.setImage(new Image("file:client/src/main/res/webadmin/back.png"));
+    }
+    @FXML
+    public void AMBack_press(MouseEvent event){
+        AMBack.setImage(new Image("file:client/src/main/res/webadmin/back_press.png"));
+    }
+    @FXML
+    public void AMBack_release(MouseEvent event){
+        AMBack.setImage(new Image("file:client/src/main/res/webadmin/back_movein.png"));
+    }
+    @FXML
+    public void AMBack_action(MouseEvent event){
+        Fade amFade=new Fade(accountManage,400,false);
+        amFade.play();
+        Fade welFadeIn=new Fade(welcome,400,true);
+        welFadeIn.play();
+    }
+    //HMBack的按钮实现
+    @FXML
+    public void HMBack_movein(MouseEvent event){
+        HMBack.setImage(new Image("file:client/src/main/res/webadmin/back_movein.png"));
+    }
+    @FXML
+    public void HMBack_moveout(MouseEvent event){
+        HMBack.setImage(new Image("file:client/src/main/res/webadmin/back.png"));
+    }
+    @FXML
+    public void HMBack_press(MouseEvent event){
+        HMBack.setImage(new Image("file:client/src/main/res/webadmin/back_press.png"));
+    }
+    @FXML
+    public void HMBack_release(MouseEvent event){
+        HMBack.setImage(new Image("file:client/src/main/res/webadmin/back_movein.png"));
+    }
+    @FXML
+    public void HMBack_action(MouseEvent event){
+        Fade hmFade=new Fade(hotelManage,400,false);
+        hmFade.play();
+        Fade welFadeIn=new Fade(welcome,400,true);
+        welFadeIn.play();
+    }
+
+    //addAccount的按钮实现
+    @FXML
+    public void addAccount_movein(MouseEvent event){
+        addAccount.setImage(new Image("file:client/src/main/res/webadmin/addAccount_movein.png"));
+    }
+    @FXML
+    public void addAccount_moveout(MouseEvent event){
+        addAccount.setImage(new Image("file:client/src/main/res/webadmin/addAccount.png"));
+    }
+    @FXML
+    public void addAccount_press(MouseEvent event){
+        addAccount.setImage((new Image("file:client/src/main/res/webadmin/addAccount_press.png")));
+    }
+    @FXML
+    public void addAccount_release(MouseEvent event){
+        addAccount.setImage(new Image("file:client/src/main/res/webadmin/addAccount_movein.png"));
+    }
+    @FXML
+    public void addAccount_action(MouseEvent event){
+        //TODO
+    }
+
+    //addHotel的按钮实现
+    @FXML
+    public void addHotel_movein(MouseEvent event){
+        addHotel.setImage(new Image("file:client/src/main/res/webadmin/addHotel_movein.png"));
+    }
+    @FXML
+    public void addHotel_moveout(MouseEvent event){
+        addHotel.setImage(new Image("file:client/src/main/res/webadmin/addHotel.png"));
+    }
+    @FXML
+    public void addHotel_press(MouseEvent event){
+        addHotel.setImage((new Image("file:client/src/main/res/webadmin/addHotel_press.png")));
+    }
+    @FXML
+    public void addHotel_release(MouseEvent event){
+        addHotel.setImage(new Image("file:client/src/main/res/webadmin/addHotel_movein.png"));
+    }
+    @FXML
+    public void addHotel_action(MouseEvent event){
+        //TODO
+    }
 }
