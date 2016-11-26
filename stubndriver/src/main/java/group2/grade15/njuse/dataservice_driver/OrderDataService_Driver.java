78cb850afe809fe2c11c7f3ece2a1587e50785b4
iriswuyuhan <1426546185@qq.com>
2016-11-26 20:31:39 +0800

    Merge branches 'Wu' and 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Wu
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPromotionListPO.java
    #	common/src/main/java/group2/grade15/njuse/po/WebPromotionListPO.java

--- stubndriver/src/main/java/group2/grade15/njuse/dataservice_driver/OrderDataService_Driver.java
+++ stubndriver/src/main/java/group2/grade15/njuse/dataservice_driver/OrderDataService_Driver.java
@@ -5,6 +5,7 @@ import group2.grade15.njuse.po.OrderPO;
 import group2.grade15.njuse.utility.IDType;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 /**
  * Created by George on 2016/10/16.
@@ -13,9 +14,9 @@ public class OrderDataService_Driver {
 
     public void drive(OrderDataService orderDataService) {
         try {
-            OrderListPO hotelPromotionListPO = orderDataService.getList(123456, IDType.customer);
-//            ResultMessage addInfo = orderDataService.add(new OrderPO());
-//            ResultMessage modifyInfo = orderDataService.modify(new OrderPO());
+            ArrayList<OrderPO> hotelPromotionListPO = orderDataService.getList(123456, IDType.customer);
+//          ResultMessage addInfo = orderDataService.add(new OrderPO());
+//          ResultMessage modifyInfo = orderDataService.modify(new OrderPO());
             OrderPO orderPO = orderDataService.get(12346);
         } catch (RemoteException e) {
             e.printStackTrace();
