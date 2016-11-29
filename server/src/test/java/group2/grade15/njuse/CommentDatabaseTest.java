46fdca8da20e8cbe546cb35d5690833ea8277159
iriswuyuhan <1426546185@qq.com>
2016-11-29 15:16:06 +0800

    Merge branches 'Wu' and 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Wu
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPO.java

--- server/src/test/java/group2/grade15/njuse/CommentDatabaseTest.java
+++ server/src/test/java/group2/grade15/njuse/CommentDatabaseTest.java
@@ -2,6 +2,8 @@ package group2.grade15.njuse;
 
 import group2.grade15.njuse.data.commentdata.CommentDatabaseImpl;
 import group2.grade15.njuse.data.databaseimpl.DatabaseInfo;
+import group2.grade15.njuse.po.CommentPO;
+import group2.grade15.njuse.utility.ResultMessage;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,4 +28,20 @@ public class CommentDatabaseTest {
         assertEquals(1,commentDatabase.getHotelComments(1).get(0).getCommentID());
     }
 
+    @Test
+    public void getCustomerCommentTest() throws RemoteException{
+        assertEquals(1,commentDatabase.getCustomerComments(1).get(0).getHotelID());
+    }
+
+    @Test
+    public void addTest() throws RemoteException{
+        CommentPO commentPO=new CommentPO(1,1,null,"Bad!",0,50.5,2);
+        assertEquals(ResultMessage.SUCCESS,commentDatabase.add(commentPO));
+    }
+
+    @Test
+    public void modifyTest() throws RemoteException{
+        CommentPO commentPO=new CommentPO(1,1,null,"Just so so",2,50.5,2);
+        assertEquals(ResultMessage.SUCCESS,commentDatabase.modify(commentPO));
+    }
 }
