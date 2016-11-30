4304d7c0f04ae851ebd5933b5f9b790796eda95e
Luo <359303713@qq.com>
2016-11-30 23:36:39 +0800

    一次同步

---
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/CreditChargeController.java
@@ -0,0 +1,40 @@
+package group2.grade15.njuse.presentation.webmarketerui;
+
+import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by ALIENWARE-PC on 2016/11/30.
+ */
+public class CreditChargeController implements Initializable {
+    @FXML
+    private Label check;
+    @FXML
+    private Label cancel;
+    @FXML
+    private TextField accountField;
+    @FXML
+    private TextField accountCheckField;
+    @FXML
+    private TextField amountField;
+    @FXML
+    private TextField PWField;
+    @FXML
+    private Label backInfo;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        CustomeButton.implButton(check,"file:client/src/main/res/webmarketer/Check");
+        CustomeButton.implButton(cancel,"file:client/src/main/res/marketer/Cancel");
+
+    }
+}
--- client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/WebMarketerMainController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/WebMarketerMainController.java
@@ -1,7 +1,21 @@
 package group2.grade15.njuse.presentation.webmarketerui;
 
+import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by ALIENWARE-PC on 2016/11/26.
  */
-public class WebMarketerMainController {
+public class WebMarketerMainController implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
