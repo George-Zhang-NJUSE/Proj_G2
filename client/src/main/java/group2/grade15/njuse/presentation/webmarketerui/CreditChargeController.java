cba162f1114efe25e754c6c2400419c1bd272d86
Luo <359303713@qq.com>
2016-12-01 00:47:58 +0800

    网站营销人员的主界面框架与充值界面

--- client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/CreditChargeController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/CreditChargeController.java
@@ -1,10 +1,13 @@
 package group2.grade15.njuse.presentation.webmarketerui;
 
+import group2.grade15.njuse.presentation.myanimation.Fade;
 import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +16,7 @@ import java.util.ResourceBundle;
  * Created by ALIENWARE-PC on 2016/11/30.
  */
 public class CreditChargeController implements Initializable {
+    public Pane motherPane;
     @FXML
     private Label check;
     @FXML
@@ -33,8 +37,18 @@ public class CreditChargeController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         CustomeButton.implButton(check,"file:client/src/main/res/webmarketer/Check");
         CustomeButton.implButton(cancel,"file:client/src/main/res/webmarketer/Cancel");
+        cancel.setOnMouseClicked((MouseEvent event)->{
+
+        });
+    }
+    private void toPromotionManage(){
+        //TODO 从充值到促销管理的界面
 
     }
+    private void toFix(){
+        //TODO 从充值到订单申诉的界面
+    };
 }
--- client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/WebMarketerMainController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/WebMarketerMainController.java
@@ -1,11 +1,15 @@
 package group2.grade15.njuse.presentation.webmarketerui;
 
+import group2.grade15.njuse.presentation.myanimation.Fade;
 import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,9 +17,25 @@ import java.util.ResourceBundle;
  * Created by ALIENWARE-PC on 2016/11/26.
  */
 public class WebMarketerMainController implements Initializable{
-
+    public Pane motherPane;
+    @FXML
+    private Pane opPane;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        toCreditCharge();
+    }
 
+    public void toCreditCharge(){
+        try{
+            FXMLLoader creditChargeLoader=new FXMLLoader(new URL("file:client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/CreditCharge.fxml"));
+            motherPane.getChildren().removeAll();
+            motherPane.getChildren().add(creditChargeLoader.load());
+            Fade fadein=new Fade(motherPane,500,true);
+            fadein.play();
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
