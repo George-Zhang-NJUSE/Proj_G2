c03e25fd4dd32a20b8796a14fa40a4152de58be6
Leeski222 <244967114@qq.com>
2016-12-01 15:34:50 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Li
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPO.java

--- client/src/main/java/group2/grade15/njuse/vo/SearchConditionVO.java
+++ client/src/main/java/group2/grade15/njuse/vo/SearchConditionVO.java
@@ -1,8 +1,24 @@
 package group2.grade15.njuse.vo;
 
+import group2.grade15.njuse.utility.RoomType;
+import group2.grade15.njuse.utility.SortMethod;
+
+import java.util.Date;
+
 /**
  * Created by Guo on 2016/11/30.
  */
 public class SearchConditionVO {
-
+    SortMethod sortBy;
+    String name;
+    RoomType roomType;
+    double minPrice;
+    double maxPrice;
+    int freeRoomNum;
+    int starLevel;
+    Date checkInTime;
+    Date checkOutTime;
+    double minScore;
+    double maxScore;
+    boolean isBooked;
 }
---
+++ common/src/main/java/group2/grade15/njuse/utility/SortMethod.java
@@ -0,0 +1,10 @@
+package group2.grade15.njuse.utility;
+
+/**
+ * Created by Guo on 2016/12/1.
+ */
+public enum SortMethod {
+    PRICE,
+    STAR_LEVEL,
+    SCORE;
+}
