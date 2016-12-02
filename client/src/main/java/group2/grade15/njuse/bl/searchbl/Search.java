0664af6a903e484e8927b35dd0e88c1e5f0a8c7f
Leeski222 <244967114@qq.com>
2016-12-02 10:35:37 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Li
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPO.java

--- client/src/main/java/group2/grade15/njuse/bl/searchbl/Search.java
+++ client/src/main/java/group2/grade15/njuse/bl/searchbl/Search.java
@@ -2,9 +2,14 @@ package group2.grade15.njuse.bl.searchbl;
 
 import group2.grade15.njuse.blservice.SearchServ;
 import group2.grade15.njuse.po.*;
+import group2.grade15.njuse.rmi.RemoteHelper;
+import group2.grade15.njuse.utility.RoomType;
+import group2.grade15.njuse.utility.SortMethod;
 import group2.grade15.njuse.vo.*;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Search implements SearchServ{
 
@@ -15,7 +20,20 @@ public class Search implements SearchServ{
 
 	@Override
 	public ArrayList<ProvinceVO> getProvince() {
-		return null;
+		ArrayList<ProvincePO> provincePOList = new ArrayList();
+		ArrayList<ProvinceVO> promotionList = new ArrayList();
+
+		try {
+			provincePOList = RemoteHelper.getInstance().getAreaDataService().getProvince();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		for(ProvincePO po : provincePOList){
+			promotionList.add(new ProvinceVO(po));
+		}
+
+		return promotionList;
 	}
 
 	@Override
@@ -35,6 +53,48 @@ public class Search implements SearchServ{
 
 	@Override
 	public ArrayList<HotelVO> getHotel(String address) {
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的isBooked对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByBooked(){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的排序选择进行排序
+	 */
+	private ArrayList<HotelVO> sort(SortMethod sortBy){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的酒店名称对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByName(String name){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的房间信息对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByRoomInfo(RoomType roomType, double minPrice, double maxPrice, int freeRoomNum){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的酒店信息对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByHotelInfo(int starLevel, double minScore, double maxScore){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的入住时间信息对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByTime(Date checkInTime, Date checkOutTime){
 		return null;
 	}
 }
