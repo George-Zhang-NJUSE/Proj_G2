b736dc43799a02dd361dc755a069b07426ec01e2
Luo <359303713@qq.com>
2016-12-02 14:49:14 +0800

    异常订单管理界面和促销策略管理界面的更新

--- client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/FixController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/FixController.java
@@ -1,7 +1,72 @@
 package group2.grade15.njuse.presentation.webmarketerui;
 
+import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
+import group2.grade15.njuse.utility.ResultMessage;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by ALIENWARE-PC on 2016/12/1.
  */
-public class FixController {
+public class FixController implements Initializable{
+    @FXML
+    private Label check;
+    @FXML
+    private Label clear;
+    @FXML
+    private TextField orderID;
+    @FXML
+    private TextField currentState;
+    @FXML
+    private TextField hotelName;
+    @FXML
+    private TextField address;
+    @FXML
+    private TextField prefCheckInTime;
+    @FXML
+    private TextField prefCheckOutTime;
+    @FXML
+    private TextField restoredCredit;
+    @FXML
+    private TextArea fixReason;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        CustomeButton.implButton(check,"file:client/src/main/res/webmarketer/Check");
+        CustomeButton.implButton(clear,"file:client/src/main/res/webmarketer/Cancel");
+    }
+
+    public void clear(){
+        orderID.setText("");
+        currentState.setText("");
+        hotelName.setText("");
+        address.setText("");
+        prefCheckInTime.setText("");
+        prefCheckOutTime.setText("");
+        restoredCredit.setText("");
+        fixReason.setText("");
+
+    }
+    public void open(int ID){
+        //TODO
+    }
+    public ResultMessage fixCommit(){
+        //TODO implement the function of committing an order fixing.
+        return null;
+    }
+    public ResultMessage showUnfixed(){
+        //TODO implements the function to show the unnormal orders which are not fixed yet;
+        return null;
+    }
+    public ResultMessage showFixed(){
+        //TODO implements the function to show the unnormal orders which are fixed;
+        return null;
+    }
 }
