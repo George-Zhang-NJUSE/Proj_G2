09408e0ded8a371a8663cec1e40f3de25298efc0
FrodoLuo <359303713@qq.com>
2016-12-02 11:52:22 +0800

    hotelManage build

---
+++ client/src/main/java/group2/grade15/njuse/presentation/hotelmanageui/HotelManageMainController.java
@@ -0,0 +1,16 @@
+package group2.grade15.njuse.presentation.hotelmanageui;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by luoy on 2016/12/2.
+ */
+public class HotelManageMainController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
