539ac1f33a40f121f5a34bf703feaa6ce9e12be7
Leeski222 <244967114@qq.com>
2016-12-02 11:41:58 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Li
    # Conflicts:
    #	common/src/main/java/group2/grade15/njuse/po/HotelPO.java

--- client/src/main/java/group2/grade15/njuse/bl/searchbl/Search.java
+++ client/src/main/java/group2/grade15/njuse/bl/searchbl/Search.java
@@ -14,8 +14,42 @@ import java.util.Date;
 public class Search implements SearchServ{
 
 	@Override
-	public ArrayList<HotelVO> getHotelBySearch(SearchConditionVO searchCondition) {
-		return null;
+	public ArrayList<HotelVO> getHotelBySearch(SearchConditionVO searchCondition, HotelListVO hotelListVO) {
+		ArrayList<HotelVO> hotelList = hotelListVO.getList();
+
+		if (searchCondition.getSortBy() != SortMethod.DEFAULT && hotelList != null) {
+			hotelList = filterByBooked(hotelList);
+		}
+
+		if (searchCondition.getSortBy() != SortMethod.DEFAULT && hotelList != null) {
+			hotelList = sort(searchCondition.getSortBy(), hotelList);
+		}
+
+		if (searchCondition.getName() != null && hotelList != null) {
+			hotelList = filterByName(searchCondition.getName(), hotelList);
+		}
+
+		if (searchCondition.getMinStarLevel() != 0 || searchCondition.getMaxStarLevel() != 5 && hotelList != null){
+			hotelList = filterByStarLevel(searchCondition.getMinStarLevel(), searchCondition.getMaxStarLevel(), hotelList);
+		}
+
+		if(searchCondition.getMinScore() != 0 || searchCondition.getMaxScore() != 10 && hotelList != null) {
+			hotelList = filterByScore(searchCondition.getMinScore(), searchCondition.getMaxScore(), hotelList);
+		}
+
+		if(searchCondition.getCheckInTime() != null || searchCondition.getCheckOutTime() != null && hotelList != null) {
+			hotelList = filterByTime(searchCondition.getCheckInTime(), searchCondition.getCheckOutTime(), hotelList);
+		}
+
+		if(searchCondition.getMinPrice() != 0 || searchCondition.getMaxPrice() != 0 && hotelList != null){
+			hotelList = filterByRoomPrice(searchCondition.getMinPrice(), searchCondition.getMaxPrice(), hotelList);
+		}
+
+		if(searchCondition.getFreeRoomNum() != 0 || searchCondition.getRoomType() != RoomType.all && hotelList != null){
+			hotelList = filterByRoomInfo(searchCondition.getRoomType(), searchCondition.getFreeRoomNum(), hotelList);
+		}
+
+		return hotelList;
 	}
 
 	@Override
@@ -38,63 +72,126 @@ public class Search implements SearchServ{
 
 	@Override
 	public ArrayList<CityVO> getCity(String provinceNum) {
-		return null;
+		ArrayList<CityPO> cityPOList = new ArrayList();
+		ArrayList<CityVO> cityList = new ArrayList();
+
+		try {
+			cityPOList = RemoteHelper.getInstance().getAreaDataService().getCity(provinceNum);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		for(CityPO po : cityPOList){
+			cityList.add(new CityVO(po));
+		}
+
+		return cityList;
 	}
 
 	@Override
 	public ArrayList<DistrictVO> getDistrict(String cityNum) {
-		return null;
+		ArrayList<DistrictPO> districtPOList = new ArrayList();
+		ArrayList<DistrictVO> districtList = new ArrayList();
+
+		try {
+			districtPOList = RemoteHelper.getInstance().getAreaDataService().getDistrict(cityNum);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		for(DistrictPO po : districtPOList){
+			districtList.add(new DistrictVO(po));
+		}
+		return districtList;
 	}
 
 	@Override
 	public ArrayList<CbdVO> getCbd(String districtNum) {
-		return null;
+		ArrayList<CbdPO> cbdPOList = new ArrayList();
+		ArrayList<CbdVO> cbdList = new ArrayList();
+
+		try {
+			cbdPOList = RemoteHelper.getInstance().getAreaDataService().getCbd(districtNum);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		for(CbdPO po : cbdPOList){
+			cbdList.add(new CbdVO(po));
+		}
+		return cbdList;
 	}
 
 	@Override
 	public ArrayList<HotelVO> getHotel(String address) {
-		return null;
+		ArrayList<HotelPO> hotelPOList = new ArrayList();
+		ArrayList<HotelVO> hotelList = new ArrayList();
+
+		try {
+			hotelPOList = RemoteHelper.getInstance().getAreaDataService().getHotelByAddress(address);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		for(HotelPO po : hotelPOList){
+			hotelList.add(new HotelVO(po));
+		}
+		return hotelList;
 	}
 
 	/**
 	 * 根据SearchCondition中的isBooked对获得的酒店列表进行一次筛选
 	 */
-	private ArrayList<HotelVO> filterByBooked(){
+	private ArrayList<HotelVO> filterByBooked(ArrayList<HotelVO> hotelList){
 		return null;
 	}
 
 	/**
 	 * 根据SearchCondition中的排序选择进行排序
 	 */
-	private ArrayList<HotelVO> sort(SortMethod sortBy){
+	private ArrayList<HotelVO> sort(SortMethod sortBy, ArrayList<HotelVO> hotelList){
 		return null;
 	}
 
 	/**
 	 * 根据SearchCondition中的酒店名称对获得的酒店列表进行一次筛选
 	 */
-	private ArrayList<HotelVO> filterByName(String name){
+	private ArrayList<HotelVO> filterByName(String name, ArrayList<HotelVO> hotelList){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的房间价格对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByRoomPrice(double minPrice, double maxPrice, ArrayList<HotelVO> hotelList){
 		return null;
 	}
 
 	/**
 	 * 根据SearchCondition中的房间信息对获得的酒店列表进行一次筛选
 	 */
-	private ArrayList<HotelVO> filterByRoomInfo(RoomType roomType, double minPrice, double maxPrice, int freeRoomNum){
+	private ArrayList<HotelVO> filterByRoomInfo(RoomType roomType, int freeRoomNum, ArrayList<HotelVO> hotelList){
 		return null;
 	}
 
 	/**
-	 * 根据SearchCondition中的酒店信息对获得的酒店列表进行一次筛选
+	 * 根据SearchCondition中的酒店星级区间对获得的酒店列表进行一次筛选
 	 */
-	private ArrayList<HotelVO> filterByHotelInfo(int starLevel, double minScore, double maxScore){
+	private ArrayList<HotelVO> filterByStarLevel(int minStarLevel, int maxStarLevel, ArrayList<HotelVO> hotelList){
+		return null;
+	}
+
+	/**
+	 * 根据SearchCondition中的酒店评分区间对获得的酒店列表进行一次筛选
+	 */
+	private ArrayList<HotelVO> filterByScore(double minScore, double maxScore, ArrayList<HotelVO> hotelList){
 		return null;
 	}
 
 	/**
 	 * 根据SearchCondition中的入住时间信息对获得的酒店列表进行一次筛选
 	 */
-	private ArrayList<HotelVO> filterByTime(Date checkInTime, Date checkOutTime){
+	private ArrayList<HotelVO> filterByTime(Date checkInTime, Date checkOutTime, ArrayList<HotelVO> hotelList){
 		return null;
 	}
 }
--- client/src/main/java/group2/grade15/njuse/blservice/SearchServ.java
+++ client/src/main/java/group2/grade15/njuse/blservice/SearchServ.java
@@ -4,9 +4,14 @@ import group2.grade15.njuse.vo.*;
 
 import java.util.ArrayList;
 
+/**
+ * 搜索的接口
+ * 根据省，市，区，商圈逐级缩小范围，直到得到最后的酒店列表
+ * 还可以根据SearchCondition中含有的数据对酒店列表进行筛选
+ */
 public interface SearchServ {
 
-    public ArrayList<HotelVO> getHotelBySearch(SearchConditionVO searchCondition);
+    public ArrayList<HotelVO> getHotelBySearch(SearchConditionVO searchCondition, HotelListVO hotelListVO);
 
 	public ArrayList<ProvinceVO> getProvince();
 
