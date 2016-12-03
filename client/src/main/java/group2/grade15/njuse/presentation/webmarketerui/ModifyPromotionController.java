338266a0db2b2138d92f3209499cc96c6142ed8f
FrodoLuo <359303713@qq.com>
2016-12-03 14:01:41 +0800

    promotionManage

--- client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/ModifyPromotionController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/webmarketerui/ModifyPromotionController.java
@@ -1,6 +1,11 @@
 package group2.grade15.njuse.presentation.webmarketerui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,8 +14,53 @@ import java.util.ResourceBundle;
  * Created by luoy on 2016/12/3.
  */
 public class ModifyPromotionController implements Initializable {
+    private int ID;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField cut;
+    @FXML
+    private ComboBox type;
+    @FXML
+    private GridPane VIP;
+    @FXML
+    private GridPane time;
+    @FXML
+    private GridPane rank;
+    @FXML
+    private ComboBox requiredRank;
+    @FXML
+    private TextField CBD;
+    @FXML
+    private DatePicker startDate;
+    @FXML
+    private DatePicker endDate;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+
+    public void setEditable(){
+        boolean sw=true;
+        name.setEditable(sw);
+        cut.setEditable(sw);
+        type.setEditable(sw);
+        requiredRank.setEditable(sw);
+        CBD.setEditable(sw);
+        startDate.setEditable(sw);
+        endDate.setEditable(sw);
+    }
+    public void setUneditable(){
+        boolean sw=false;
+        name.setEditable(sw);
+        cut.setEditable(sw);
+        type.setEditable(sw);
+        requiredRank.setEditable(sw);
+        CBD.setEditable(sw);
+        startDate.setEditable(sw);
+        endDate.setEditable(sw);
     }
 }
