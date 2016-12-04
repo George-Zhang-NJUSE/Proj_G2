95373e122fc57613de55e61ac80c060b85de8e78
George-Zhang-NJUSE <georgezhang275@icloud.com>
2016-12-04 16:50:15 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2
    # Conflicts:
    #	server/pom.xml

--- client/src/main/java/group2/grade15/njuse/presentation/hotelui/CommentItemController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/hotelui/CommentItemController.java
@@ -1,7 +1,21 @@
 package group2.grade15.njuse.presentation.hotelui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 /**
  * Created by George on 2016/12/1.
  */
 public class CommentItemController {
+
+    @FXML
+    private Label scoreLabel, customerNameLabel, timeLabel;
+
+    @FXML
+    private TextArea commentArea;
+
+    public void initData() {
+
+    }
 }
