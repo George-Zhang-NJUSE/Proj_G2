6f0a3b5c2ed62e18e4319db6e481924eb54bb3e7
George-Zhang-NJUSE <georgezhang275@icloud.com>
2016-12-05 23:07:58 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2
    # Conflicts:
    #	server/pom.xml

--- client/src/main/java/group2/grade15/njuse/presentation/customerui/CustomerInfoController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/customerui/CustomerInfoController.java
@@ -1,11 +1,14 @@
 package group2.grade15.njuse.presentation.customerui;
 
+import group2.grade15.njuse.presentation.myanimation.Fade;
 import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
+import group2.grade15.njuse.vo.CustomerVO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -19,26 +22,68 @@ import java.util.ResourceBundle;
  */
 public class CustomerInfoController implements Initializable{
 
+    private CustomerVO customerVO;
+
+    @FXML
+    private Node rootNode;
+
     @FXML
     private VBox creditRecordBox;
 
     @FXML
+    private TextField userNameField, phoneNumberField;
+
+    @FXML
     private Label editUserNameLabel, editPhoneNumberLabel, cancelLabel, confirmLabel;
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //加载按钮变化样式
-        CustomeButton.implButton(editUserNameLabel, "file:client/src/main/res/customer/edit");
-        CustomeButton.implButton(editPhoneNumberLabel, "file:client/src/main/res/customer/edit");
-        CustomeButton.implButton(cancelLabel, "file:client/src/main/res/customer/cancel");
-        CustomeButton.implButton(confirmLabel, "file:client/src/main/res/customer/confirm");
-
-        showCreditHistory();
-
+    @FXML
+    private void requestEditUserName() {
+        userNameField.setEditable(true);
+        showControlButtons();
+        userNameField.requestFocus();
     }
 
-    private void showCreditHistory() {
+    @FXML
+    private void requestEditPhoneNumber() {
+        phoneNumberField.setEditable(true);
+        showControlButtons();
+        phoneNumberField.requestFocus();
+    }
+
+    @FXML
+    private void saveEdition() {
+        // TODO: 2016/12/5 更新客户信息
+
+        userNameField.setEditable(false);
+        phoneNumberField.setEditable(false);
+        hideControlButtons();
+    }
+
+    @FXML
+    private void cancelEdition() {
+
+        userNameField.setEditable(false);
+        phoneNumberField.setEditable(false);
+        hideControlButtons();
+    }
+
+
+    private void showControlButtons() {
+        cancelLabel.setVisible(true);
+        confirmLabel.setVisible(true);
+    }
+
+    private void hideControlButtons() {
+        cancelLabel.setVisible(false);
+        confirmLabel.setVisible(false);
+    }
+
+    private void show() {
+        Fade fadeIn = new Fade(rootNode, 300, true);
+        fadeIn.play();
+    }
+
+    private void loadCreditHistory() {
         try {
             creditRecordBox.getChildren().clear();
             ArrayList<Node> ItemList = new ArrayList<>();
@@ -60,4 +105,25 @@ public class CustomerInfoController implements Initializable{
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //加载按钮变化样式
+        CustomeButton.implButton(editUserNameLabel, "file:client/src/main/res/customer/edit");
+        CustomeButton.implButton(editPhoneNumberLabel, "file:client/src/main/res/customer/edit");
+        CustomeButton.implButton(cancelLabel, "file:client/src/main/res/customer/cancel");
+        CustomeButton.implButton(confirmLabel, "file:client/src/main/res/customer/confirm");
+
+        //为渐入动画做准备
+        rootNode.setOpacity(0);
+    }
+
+    public void initDataAndShow(CustomerVO vo) {
+        // TODO: 2016/12/5 加载数据
+
+        loadCreditHistory();
+
+        show();
+    }
+
 }
