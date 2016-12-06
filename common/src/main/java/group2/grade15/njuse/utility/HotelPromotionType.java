7f418a89550089d3e515a89a00639b6e10db8950
iriswuyuhan <1426546185@qq.com>
2016-12-06 17:15:06 +0800

    Merge branches 'Wu' and 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2 into Wu
    # Conflicts:
    #	stubndriver/src/main/java/group2/grade15/njuse/dataservice_driver/WebPromotionDataService_Driver.java

--- common/src/main/java/group2/grade15/njuse/utility/HotelPromotionType.java
+++ common/src/main/java/group2/grade15/njuse/utility/HotelPromotionType.java
@@ -4,8 +4,8 @@ package group2.grade15.njuse.utility;
  * Created by dell on 2016/12/1.
  */
 public enum HotelPromotionType {
+    TimeHotel,
     BirthdayHotel,
     MultipleHotel,
-    PartnerHotel,
-    TimeHotel,
+    PartnerHotel
 }
