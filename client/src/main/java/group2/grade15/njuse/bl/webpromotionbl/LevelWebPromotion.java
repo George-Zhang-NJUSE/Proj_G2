0d93f9b7929451f9a8e898121663a05e67ace5a1
Leeski222 <244967114@qq.com>
2016-12-09 00:17:57 +0800

    update

--- client/src/main/java/group2/grade15/njuse/bl/webpromotionbl/LevelWebPromotion.java
+++ client/src/main/java/group2/grade15/njuse/bl/webpromotionbl/LevelWebPromotion.java
@@ -1,7 +1,17 @@
 package group2.grade15.njuse.bl.webpromotionbl;
 
+import group2.grade15.njuse.bl.hotelbl.Hotel;
+import group2.grade15.njuse.bl.hotelbl.HotelBL;
 import group2.grade15.njuse.bl.promotionfactory.WebPromotionBL;
+import group2.grade15.njuse.rmi.RemoteHelper;
+import group2.grade15.njuse.utility.RoomType;
+import group2.grade15.njuse.vo.HotelVO;
+import group2.grade15.njuse.vo.OrderVO;
+import group2.grade15.njuse.vo.RoomVO;
 import group2.grade15.njuse.vo.WebPromotionVO;
+
+import java.sql.Date;
+import java.util.ArrayList;
 
 /**
  * Created by Guo on 2016/12/6.
@@ -9,7 +19,32 @@ import group2.grade15.njuse.vo.WebPromotionVO;
 public class LevelWebPromotion implements WebPromotionBL {
 
     @Override
-    public double countPrice(double originalPrice, WebPromotionVO webPromotionVO) {
-        return 0;
+    public double countPrice(OrderVO orderVO, WebPromotionVO webPromotionVO) {
+        int roomNum = orderVO.getRoomSum();
+        int hotelID = orderVO.getHotelID();
+        RoomType roomType = orderVO.getType();
+
+        HotelBL hotel = new Hotel();
+        double roomPrice = -1;
+
+        HotelVO hotelVO = hotel.getInfo(hotelID);
+        ArrayList<RoomVO> roomList = hotelVO.getRoomList();
+        for (RoomVO room : roomList) {
+            if (room.getType() == roomType) {
+                roomPrice = room.getPrice();
+            }
+        }
+
+        double totalPrice = roomPrice * roomNum;
+
+        if(isFit(orderVO, webPromotionVO)) {
+            return totalPrice * webPromotionVO.getDiscount();
+        } else {
+            return totalPrice;
+        }
+    }
+
+    private boolean isFit(OrderVO orderVO, WebPromotionVO webPromotionVO){
+        return true;
     }
 }
