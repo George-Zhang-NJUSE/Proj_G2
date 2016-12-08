a1bec7c53707cb4057d1ba5f348a06cc33f48dbf
George-Zhang-NJUSE <georgezhang275@icloud.com>
2016-12-08 15:37:45 +0800

    Merge branch 'master' of https://github.com/George-Zhang-NJUSE/Proj_G2
    # Conflicts:
    #	server/pom.xml

--- client/src/main/java/group2/grade15/njuse/presentation/hotelui/HotelDetailController.java
+++ client/src/main/java/group2/grade15/njuse/presentation/hotelui/HotelDetailController.java
@@ -1,11 +1,17 @@
 package group2.grade15.njuse.presentation.hotelui;
 
+import group2.grade15.njuse.bl.orderbl.OrderController;
+import group2.grade15.njuse.blservice.CommentServ;
+import group2.grade15.njuse.blservice.OrderListServ;
 import group2.grade15.njuse.presentation.customerglobal.CommonData;
 import group2.grade15.njuse.presentation.myanimation.Fade;
 import group2.grade15.njuse.presentation.myanimation.Pop;
 import group2.grade15.njuse.presentation.mycontrol.CustomeButton;
 import group2.grade15.njuse.presentation.orderui.MakeOrderController;
 import group2.grade15.njuse.presentation.orderui.MyOrderItemController;
+import group2.grade15.njuse.vo.CommentVO;
+import group2.grade15.njuse.vo.HotelVO;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -25,7 +31,11 @@ import java.util.ResourceBundle;
  */
 public class HotelDetailController implements Initializable {
 
+    private HotelVO hotelVO;
     private Pane parentPane;
+    private CommentServ commentServ;
+    private OrderListServ orderListServ;
+
 
     @FXML
     private Node rootNode;
@@ -34,7 +44,7 @@ public class HotelDetailController implements Initializable {
     private VBox commentBox, myOrderBox;
 
     @FXML
-    private Label returnLabel, makeOrderLabel;
+    private Label returnLabel, makeOrderLabel, hotelNameLabel, starLabel, addressLabel, introLabel;
 
     @FXML
     protected void goBack() {
@@ -57,59 +67,68 @@ public class HotelDetailController implements Initializable {
         }
     }
 
-    public void initDataAndShow() {
-        loadComments();
-        loadMyOrders();
+    public void initDataAndShow(HotelVO vo) {
+        hotelVO = vo;
+        if (hotelVO != null) {
+            hotelNameLabel.setText(hotelVO.getName());
+            starLabel.setText(Integer.toString(hotelVO.getRank()));
+            addressLabel.setText(hotelVO.getConcreteAddress());
+            introLabel.setText(hotelVO.getIntroduction());
+        }else{
+            System.out.println("没有酒店数据");
+        }
+
         show();
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //加载按钮变化样式
-        CustomeButton.implButton(returnLabel, "file:client/src/main/res/customer/back");
-        CustomeButton.implButton(makeOrderLabel, "file:client/src/main/res/hoteldetail/makeorder");
-
-        //为渐入扩大动画做准备
-        rootNode.setOpacity(0);
-        rootNode.setScaleX(0.9);
-        rootNode.setScaleY(0.9);
-
-        //设置父界面
-        parentPane = CommonData.getInstance().getFunctionAreaPane();
-        parentPane.getChildren().add(rootNode);
-
     }
 
     private void show() {
         //渐入扩大动画
         Fade fadeIn = new Fade(rootNode, 300, true);
         Pop popIn = new Pop(rootNode, 300, true);
+        popIn.setOnFinished((ActionEvent e) -> {
+            loadComments();
+            loadMyOrders();
+        });
+
         fadeIn.play();
         popIn.play();
 
     }
 
     private void loadComments() {
-        try {
-            commentBox.getChildren().clear();
-            ArrayList<Node> ItemList = new ArrayList<>();
 
-            // TODO: 2016/12/2 需要更改为正确的逻辑
-            for (int i = 0; i < 15; ++i) {
-                FXMLLoader loader = new FXMLLoader(new URL("file:client/src/main/java/group2/grade15/njuse/presentation/hotelui/CommentItem.fxml"));
-                Node singleItemTemplate = loader.load();
-                CommentItemController commentItemController = loader.getController();
+        if (hotelVO != null) {
+            ArrayList<CommentVO> commentVOList = commentServ.getHotelCommentList(hotelVO.getId()).getList();
 
-                ItemList.add(singleItemTemplate);
+
+
+
+        } else {
+
+            //用于本地测试
+
+            try {
+                commentBox.getChildren().clear();
+                ArrayList<Node> ItemList = new ArrayList<>();
+
+
+                for (int i = 0; i < 15; ++i) {
+                    FXMLLoader loader = new FXMLLoader(new URL("file:client/src/main/java/group2/grade15/njuse/presentation/hotelui/CommentItem.fxml"));
+                    Node singleItemTemplate = loader.load();
+                    CommentItemController commentItemController = loader.getController();
+
+                    ItemList.add(singleItemTemplate);
+                }
+
+                commentBox.getChildren().addAll(ItemList);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            commentBox.getChildren().addAll(ItemList);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
     private void loadMyOrders() {
@@ -135,6 +154,27 @@ public class HotelDetailController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        commentServ = new group2.grade15.njuse.bl.commentbl.CommentController();
+        orderListServ = new OrderController();
+
+        //加载按钮变化样式
+        CustomeButton.implButton(returnLabel, "file:client/src/main/res/customer/back");
+        CustomeButton.implButton(makeOrderLabel, "file:client/src/main/res/hoteldetail/makeorder");
+
+        //为渐入扩大动画做准备
+        rootNode.setOpacity(0);
+        rootNode.setScaleX(0.9);
+        rootNode.setScaleY(0.9);
+
+        //设置父界面
+        parentPane = CommonData.getInstance().getFunctionAreaPane();
+        parentPane.getChildren().add(rootNode);
+
     }
 
 }
