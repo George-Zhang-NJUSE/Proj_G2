f24b3e177dc951f684de59f051590bfed5efe99f
Leeski222 <244967114@qq.com>
2016-12-09 11:58:07 +0800

    update

--- client/src/main/java/group2/grade15/njuse/bl/orderbl/OrderBL.java
+++ client/src/main/java/group2/grade15/njuse/bl/orderbl/OrderBL.java
@@ -1,12 +1,10 @@
 package group2.grade15.njuse.bl.orderbl;
 
-import group2.grade15.njuse.po.OrderPO;
-import group2.grade15.njuse.rmi.RemoteHelper;
 import group2.grade15.njuse.vo.OrderVO;
 
-import java.rmi.RemoteException;
 
 public interface OrderBL {
     public OrderVO getInfo(int orderID);
 
+    public double getOriginalPrice(OrderVO orderVO);
 }
--- client/src/main/java/group2/grade15/njuse/bl/webpromotionbl/TimeWebPromotion.java
+++ client/src/main/java/group2/grade15/njuse/bl/webpromotionbl/TimeWebPromotion.java
@@ -1,37 +1,26 @@
 package group2.grade15.njuse.bl.webpromotionbl;
 
-import group2.grade15.njuse.bl.hotelbl.Hotel;
-import group2.grade15.njuse.bl.hotelbl.HotelBL;
+import group2.grade15.njuse.bl.orderbl.Order;
+import group2.grade15.njuse.bl.orderbl.OrderBL;
 import group2.grade15.njuse.bl.promotionfactory.WebPromotionBL;
-import group2.grade15.njuse.utility.RoomType;
 import group2.grade15.njuse.vo.*;
 
 import java.sql.Date;
-import java.util.ArrayList;
 
 /**
  * Created by Guo on 2016/12/6.
  */
 public class TimeWebPromotion implements WebPromotionBL {
+    public OrderBL order;
+
+    public TimeWebPromotion(){
+        order = new Order();
+    }
 
     @Override
     public double countPrice(OrderVO orderVO, WebPromotionVO webPromotionVO) {
-        int roomNum = orderVO.getRoomSum();
-        int hotelID = orderVO.getHotelID();
-        RoomType roomType = orderVO.getType();
 
-        HotelBL hotel = new Hotel();
-        double roomPrice = -1;
-
-        HotelVO hotelVO = hotel.getInfo(hotelID);
-        ArrayList<RoomVO> roomList = hotelVO.getRoomList();
-        for (RoomVO room : roomList) {
-            if (room.getType() == roomType) {
-                roomPrice = room.getPrice();
-            }
-        }
-
-        double totalPrice = roomPrice * roomNum;
+        double totalPrice = order.getOriginalPrice(orderVO);
 
         if(isFit(orderVO, webPromotionVO)) {
             return totalPrice * webPromotionVO.getDiscount();
